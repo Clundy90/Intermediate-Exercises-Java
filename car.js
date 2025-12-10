20e784f9a27f11f2d1ce7b4f06f7468dc3b6b58f
Clundy90 <roetemeteor2@gmail.com>
2025-12-10 11:02:46 -0600

    Add files via upload

---
+++ car.js
@@ -0,0 +1,71 @@
+class Vehicle {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  honk() {
+    return "Beep.";
+  }
+
+  toString() {
+    return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+  }
+}
+let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+myFirstVehicle.honk(); // "Beep."
+myFirstVehicle.toString(); // "The vehicle is a Honda Monster Truck from 1999."
+console.log(myFirstVehicle.toString());
+console.log(myFirstVehicle.honk());
+
+class Car extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 4;
+  }
+}
+
+let myFirstCar = new Car("Toyota", "Corolla", 2005);
+myFirstCar.toString(); // "The vehicle is a Toyota Corolla from 2005."
+myFirstCar.honk(); // "Beep."
+myFirstCar.numWheels; // 4
+console.log(myFirstCar.toString());
+console.log(myFirstCar.honk());
+console.log(myFirstCar.numWheels);
+
+class Motorcycle extends Vehicle {
+  constructor(make, model, year) {
+    super(make, model, year);
+    this.numWheels = 2;
+  }
+  revEngine() {
+    console.log("VROOM!!!");
+  }
+}
+
+class Garage {
+  constructor(capacity) {
+    this.vehicles = [];
+    this.capacity = capacity;
+  }
+
+  add(newVehicle) {
+    if (!(newVehicle instanceof Vehicle)) {
+      return "Only vehicles are allowed in here!";
+    }
+    if (this.vehicles.length >= this.capacity) {
+      return "Sorry, we're full.";
+    }
+    this.vehicles.push(newVehicle);
+    return "Vehicle added!";
+  }
+}
+
+let garage = new Garage(2);
+console.log(garage.vehicles); // []
+
+console.log(garage.add(new Car("Hyundai", "Elantra", 2015))); // "Vehicle added!"
+console.log(garage.vehicles); // [Car]
+console.log(garage.add(new Motorcycle("Honda", "Nighthawk", 2000))); // "Vehicle added!"
+console.log(garage.vehicles);
+console.log(garage.add(new Motorcycle("Harley Davidson", "Street 750", 2019))); // "Sorry, we're full."
